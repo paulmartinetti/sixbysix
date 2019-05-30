6caee2d6516f6395c9c194706760814e43f998bc
Paul Martinetti <paul@digscitech.com>
2019-05-30 14:17:08 -0400

    working, loads all photos in sixBySix

--- js/scenes/gameScene.js
+++ js/scenes/gameScene.js
@@ -1,11 +1,6 @@
 // create a new scene
 let gameScene = new Phaser.Scene('Game');
 
-/* game flow
-*  
-*
-*/
-
 // some parameters for our scene
 gameScene.init = function () {
 
@@ -15,7 +10,7 @@ gameScene.init = function () {
     this.btnW = 262;
     this.btnH = 53;
 
-    this.topPhoto = 1;
+    this.curPhoto = 0;
     this.iga = 3;
     this.fitz = 3;
 
@@ -25,25 +20,60 @@ gameScene.init = function () {
 gameScene.create = function () {
 
     // btns
-    this.t_btn = this.add.sprite(this.gameW/2, this.btnH * 2, 'btn').setDepth(500).setInteractive();
+    let btnDepth = 500;
+    this.t_btn = this.add.sprite(this.gameW / 2, this.btnH / 2, 'btn').setDepth(btnDepth).setInteractive();
     this.t_btn.on('pointerdown', function () {
         this.iga--;
         this.nextPhoto();
-    }, this)
+    }, this);
 
-    this.b_btn = this.add.sprite(this.gameW/2, this.gameH - this.btnH, 'btn').setDepth(500).setInteractive();
+    this.b_btn = this.add.sprite(this.gameW / 2, this.gameH - this.btnH / 2, 'btn').setDepth(btnDepth).setInteractive();
     this.b_btn.angle = 180;
+    this.b_btn.on('pointerdown', function (){
+        this.iga++;
+        this.nextPhoto();
+    }, this);
+
+    this.l_btn = this.add.sprite(this.btnH / 2, this.gameH / 2, 'btn').setDepth(btnDepth).setInteractive();
+    this.l_btn.angle = -90;
+    this.l_btn.on('pointerdown', function (){
+        this.fitz--;
+        this.nextPhoto();
+    }, this);
+
+    this.r_btn = this.add.sprite(this.gameW-this.btnH/2, this.gameH/2, 'btn').setDepth(btnDepth).setInteractive();
+    this.r_btn.angle = 90;
+    this.r_btn.on('pointerdown', function (){
+        this.fitz++;
+        this.nextPhoto();
+    }, this);
+
+    // init
+    this.nextPhoto();
 
 };
 gameScene.nextPhoto = function () {
     // all btns.visible = true;
+    this.t_btn.visible = true;
+    this.b_btn.visible = true;
+    this.l_btn.visible = true;
+    this.r_btn.visible = true;
 
-    //this.topPhoto++;
     let concat = 'p' + this.fitz + '' + this.iga;
-    let curPhoto = this.add.sprite(0, 0, concat).setOrigin(0, 0).setDepth(1);
+    if (this.curPhoto) this.curPhoto.destroy();
+    this.curPhoto = this.add.sprite(0, 0, concat).setOrigin(0, 0);
 
-    // hide certain buttons
+    // update nav
     if (this.iga < 1) {
         this.t_btn.visible = false;
+    }
+    if (this.iga > 4) {
+        this.b_btn.visible = false;
+    }
+    if (this.fitz < 2) {
+        this.l_btn.visible = false;
+    }
+    if (this.fitz > 5) {
+        this.r_btn.visible = false;
     }
 }
